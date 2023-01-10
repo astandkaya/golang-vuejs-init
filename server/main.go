5931b1fb4aad22025da9cf5567f77382ee520e9a
ast <ast@over-sky.net>
2023-01-10 21:49:58 +0900

    Remove unnecessary routing

--- server/main.go
+++ server/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 
     "app/database"
@@ -17,12 +15,6 @@ func main() {
     // -----
     // route
     // -----
-    g.GET("/", func(c *gin.Context) {
-        c.JSON(http.StatusOK, gin.H{
-            "message": "hello world",
-        })
-    })
-
     test := g.Group("/test")
     {
         v1 := test.Group("/v1")
